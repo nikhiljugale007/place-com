847c8487f1637d0d2270bb6f19576462f20123ed
nikhiljugale007 <nikhiljugale007@gmail.com>
2022-05-13 14:28:18 +0530

    added admin route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import "./App.css";
 import { Routes, Route } from "react-router-dom";
 import Home from "./pages/home/Home";
+import Mockman from "mockman-js";
+
 import { Sidebar, MobileNavigation, Header, RequireAuth } from "./components";
 import AppliedDrives from "./pages/applied-drives/AppliedDrives";
 import { Login } from "./pages/authentication/Login";
@@ -10,38 +12,39 @@ import AllDrives from "./pages/all-drives/AllDrives";
 import UpcomingDrives from "./pages/upcoming-drives/UpcomingDrives";
 import SingleDrive from "./pages/single-drive/SingleDrive";
 function App() {
-	return (
-		<div className="home-page-container">
-			<Header />
-			<div className="content-section">
-				<aside className="aside">
-					<Sidebar />
-				</aside>
-				<div className="main-section">
-					<Routes>
-						<Route path="/" element={<Home />} />
-						<Route
-							path="/profile"
-							element={
-								<RequireAuth from="/profile">
-									<Profile />
-								</RequireAuth>
-							}
-						/>
-						<Route path="/drives" element={<AllDrives />} />
-						<Route path="/drives/:driveId" element={<SingleDrive />} />
-						<Route path="/applied-drives" element={<AppliedDrives />} />
-						<Route path="/upcoming-drives" element={<UpcomingDrives />} />
-						<Route path="/login" element={<Login />} />
-						<Route path="/signup" element={<Signup />} />
-					</Routes>
-				</div>
-			</div>
-			<div className="mobile-nav">
-				<MobileNavigation />
-			</div>
-		</div>
-	);
+  return (
+    <div className="home-page-container">
+      <Header />
+      <div className="content-section">
+        <aside className="aside">
+          <Sidebar />
+        </aside>
+        <div className="main-section">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route
+              path="/profile"
+              element={
+                <RequireAuth from="/profile">
+                  <Profile />
+                </RequireAuth>
+              }
+            />
+            <Route path="/drives" element={<AllDrives />} />
+            <Route path="/drives/:driveId" element={<SingleDrive />} />
+            <Route path="/applied-drives" element={<AppliedDrives />} />
+            <Route path="/upcoming-drives" element={<UpcomingDrives />} />
+            <Route path="/login" element={<Login />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/admin" element={<Mockman />} />
+          </Routes>
+        </div>
+      </div>
+      <div className="mobile-nav">
+        <MobileNavigation />
+      </div>
+    </div>
+  );
 }
 
 export default App;
